a832995522500b2b5de49e6a985e9f972ca2f3a9
uorhsohk <msajjadkhoshrou@gmail.com>
2020-12-23 19:00:42 +0100

    Listing 1.3 and Listing 1.4

---
+++ Chapter_01/src/test/java/BraveKnightTest.java
@@ -0,0 +1,17 @@
+package test.java;
+
+import main.java.BraveKnight;
+import main.java.Quest;
+import org.junit.Test;
+
+import static org.mockito.Mockito.*;
+
+public class BraveKnightTest {
+    @Test
+    public void knightShouldEmbarkOnQuest() {
+        Quest mockQuest = mock(Quest.class);
+        BraveKnight knight = new BraveKnight(mockQuest);
+        knight.embarkOnQuest();
+        verify(mockQuest, times(1)).embark();
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # spring-in-action-4th-edition
 Spring in Action 4th Edition
+
+Leider waren manche Beispiele schwert zu verstehen, von daher, habe ich mich entschieden, das Ganze in einem Git-Repo hochzuladen, um es evtl. für manche verständlicher wird.
