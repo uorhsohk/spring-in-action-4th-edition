aca7eb9913367eb580d917b68d6c7961e8503791
uorhsohk <msajjadkhoshrou@gmail.com>
2021-01-01 18:26:17 +0100

    chapter 5

---
+++ Chapter_05/src/main/java/spittr/config/RootConfig.java
@@ -0,0 +1,13 @@
+package main.java.spittr.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration
+@ComponentScan(basePackages = {"main.java.spittr"}, excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)
+})
+public class RootConfig {
+}
